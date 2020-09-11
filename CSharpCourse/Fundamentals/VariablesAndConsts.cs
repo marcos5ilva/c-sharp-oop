7fa4a9036079858b9c46a987f3104fd9fe4901f8
Marcos Silva <marcos.silva79@gmail.com>
2020-09-11 15:19:32 -0400

    feat: add Fundamentals.VariablesAndConsts class

---
+++ CSharpCourse/Fundamentals/VariablesAndConsts.cs
@@ -0,0 +1,10 @@
+namespace CSharpCourse.Fundamentals
+{
+    public class VariablesAndConsts
+    {
+        public static void Execute()
+        {
+            System.Console.WriteLine("Variable and cost class");
+        }
+    }
+}
